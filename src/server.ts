14ce4e476820381bcb9b1f35ec9921ce32b5ac77
projeto Libras <librasappsenac@gmail.com>
2021-08-31 18:13:50 -0300

    aula 02 45%

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,7 @@
+import "reflect-metadata";
 import express from "express";
-
+import "./database";
 const app = express();
-
-app.get("/test", (req, res) => {
-   return res.send("ola MUNDO")
-});
-
-app.post("/test-post", (req, res) =>{
-    return res.send("Metodo POST")
-});
 
 
 app.listen(3000, () => console.log("Server is running"))
