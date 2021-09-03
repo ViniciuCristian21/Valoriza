1820d20d251ed43123016d44ef5ac12d1d0a5154
Viniciu Cristian <git.viniciu@gmail.com>
2021-09-03 17:02:07 -0300

    aula 03 55%

--- src/database/migrations/1630695200980-CreateTags.ts
+++ src/database/migrations/1630695200980-CreateTags.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1630442284334 implements MigrationInterface {
+export class CreateTags1630695200980 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "users",
+                name: "tags",
                 columns: [
                     {
                         name: "id",
@@ -14,16 +14,7 @@ export class CreateUsers1630442284334 implements MigrationInterface {
                     },
                     {
                         name: "name",
-                        type: "varchar"
-                    },
-                    {
-                        name: "email",
-                        type: "varchar"
-                    },
-                    {
-                        name: "admin",
-                        type: "boolean",
-                        default: false
+                        type: "varchar",
                     },
                     {
                         name: "created_at",
@@ -34,14 +25,14 @@ export class CreateUsers1630442284334 implements MigrationInterface {
                         name: "updated_at",
                         type: "timestamp",
                         default: "now()"
-                    },
+                    }
                 ]
             })
         )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("users");
+        await queryRunner.dropTable("tags");
     }
 
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 const router = Router();
 
 
 const crateUsersController = new CreateUserController();
+const createTagController = new CreateTagController();
 
+router.post("/tags", createTagController.handle)
 
 router.post("/users", crateUsersController.handle);
 
