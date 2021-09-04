c6fdc47d900acfddfab3387f1aca4612ea9a76f1
Viniciu Cristian <git.viniciu@gmail.com>
2021-09-04 13:07:42 -0300

    aula 03 45%

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
-
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 const router = Router();
 
 
 const crateUsersController = new CreateUserController();
 const createTagController = new CreateTagController();
 
-router.post("/tags", createTagController.handle)
-
+router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/users", crateUsersController.handle);
 
 export { router };
